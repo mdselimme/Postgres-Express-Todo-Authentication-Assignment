64c12a4a6e1b25f08dbd1db5f2944ae6c3ccdca2
mdselimme <mdselimdev@gmail.com>
2025-03-23 17:55:09 +0600

    add login method and jsobtoken key

--- routes/todos.js
+++ routes/todos.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db = require("../config/db");
 const bcrypt = require('bcrypt');
-
+const jwt = require('jsonwebtoken');
 
 
 // Create User Registration 
@@ -33,8 +33,43 @@ router.post('/register', async (req, res) => {
     console.log(err)
     res.status(500).json({ message: "Server Internal Error" });
   }
+});
 
-})
+// Log In User 
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  console.log(req.body);
+  if (!email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  try {
+    // Email Compare Check 
+    const userEmailExits = await db.query(`SELECT * FROM users WHERE email = $1`, [email]);
+    if (userEmailExits.rows.length === 0) {
+      return res.status(400).json({ message: "Invalid Email Or Password" });
+    }
+
+    // Password Compare 
+    const passMathch = await bcrypt.compare(password, userEmailExits.rows[0].password);
+    if (!passMathch) {
+      return res.status(400).json({ message: "Invalid Email Or Password" });
+    }
+
+    // Generate Json Web Token 
+    const webToken = jwt.sign(
+      { id: userEmailExits.rows[0].id, email: userEmailExits.rows[0].email },
+      process.env.JWT_TOKEN_SECRET,
+      { expiresIn: '1h' }
+    );
+
+    res.status(200).json({ message: "Login successful", data: { id: userEmailExits.rows[0].id, name: userEmailExits.rows[0].name, email: userEmailExits.rows[0].email }, webToken });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Internal Error" });
+  };
+});
+
 
 // GET all todos
 router.get("/", async (req, res, next) => {
