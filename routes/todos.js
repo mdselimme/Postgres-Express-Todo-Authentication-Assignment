60dd0748edd2d399a4a4599bedd79907b6c9bc89
mdselimme <mdselimdev@gmail.com>
2025-03-25 22:31:13 +0600

    add swagger ui

--- routes/todos.js
+++ routes/todos.js
@@ -1,76 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../config/db");
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const authenticateToken = require("../middleswares/middlewares")
-
-
-// Create User Registration 
-router.post('/register', async (req, res) => {
-  console.log(req.body);
-  const { name, email, password } = req.body;
-  if (!name || !email || !password) {
-    return res.status(400).json({ message: "All feilds are required" });
-  }
-  try {
-    // Check Email Duplicate 
-    const checkEmail = await db.query(`SELECT id FROM users WHERE email = $1`, [email]);
-    if (checkEmail.rows.length > 0) {
-      return res.status(400).json({ message: "Already Have An Account" });
-    };
-
-    // Hashpassword method 
-    const sugerLimit = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(password, sugerLimit);
-
-    // User Registration method 
-    const createUser = await db.query(
-      `INSERT INTO users (name, email, password) VALUES($1, $2, $3) RETURNING id, name, email`, [name, email, hashPassword]
-    )
-    res.status(201).json({ message: "User Created Successfully", user: createUser.rows[0] });
-
-  } catch (err) {
-    console.log(err)
-    res.status(500).json({ message: "Server Internal Error" });
-  }
-});
-
-// Log In User 
-router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-  console.log(req.body);
-  if (!email || !password) {
-    return res.status(400).json({ message: "All fields are required" });
-  }
-  try {
-    // Email Compare Check 
-    const userEmailExits = await db.query(`SELECT * FROM users WHERE email = $1`, [email]);
-    if (userEmailExits.rows.length === 0) {
-      return res.status(400).json({ message: "Invalid Email Or Password" });
-    }
-
-    // Password Compare 
-    const passMathch = await bcrypt.compare(password, userEmailExits.rows[0].password);
-    if (!passMathch) {
-      return res.status(400).json({ message: "Invalid Email Or Password" });
-    }
-
-    // Generate Json Web Token 
-    const webToken = jwt.sign(
-      { id: userEmailExits.rows[0].id, email: userEmailExits.rows[0].email },
-      process.env.JWT_TOKEN_SECRET,
-      { expiresIn: '1h' }
-    );
-
-    res.status(200).json({ message: "Login successful", data: { id: userEmailExits.rows[0].id, name: userEmailExits.rows[0].name, email: userEmailExits.rows[0].email }, webToken });
-
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Server Internal Error" });
-  };
-});
-
+const authenticateToken = require("../controller/middlewares")
 
 
 // GET all todos
--- server.js
+++ server.js
@@ -2,21 +2,56 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require('cors');
 const app = express();
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 3000;
 require("dotenv").config();
 
-app.use(cors())
-
-const todoRoutes = require("./routes/todos");
 
 
+
+app.use(cors({
+  credentials: true
+}))
 
 // Middleware
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cookieParser());
+
+const todoRoutes = require("./routes/todos");
+const authRoutes = require("./controller/authController");
+
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'TODO API',
+      version: '1.0.0',
+      description: 'API documentation for the TODO website',
+      contact: {
+        name: 'Md Selim',
+        email: 'contact.mdselim.dev@gmail.com'
+      }
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Local server'
+      }
+    ]
+  },
+  apis: ['*js']
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Routes
 app.use("/api/todos", todoRoutes);
+app.use("/api/user", authRoutes);
 
 // Root route
 app.get("/", (req, res) => {
