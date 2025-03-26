81038cbc9476d88ce512f5191de7790c305eadde
mdselimme <mdselimdev@gmail.com>
2025-03-26 14:38:35 +0600

    add update middlewares

--- controller/middlewares.js
+++ controller/middlewares.js
@@ -7,6 +7,8 @@ const authenticateToken = (req, res, next) => {
 
     const token = req.header("Authorization")?.split(" ")[1];
 
+    console.log("token = ", token)
+
     if (!token) return res.status(401).json({ message: "Access Denied: No token Provided" });
 
     try {
