7c99d8e083d5a56e26a28712103b88f20c4378fb
mdselimme <mdselimdev@gmail.com>
2025-03-26 16:20:04 +0600

    swagger css add

--- server.js
+++ server.js
@@ -16,10 +16,13 @@ app.use(cors({
   origin: ["http://localhost:5173"]
 }));
 
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css";
 
 
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+  customCss: '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+  customCssUrl: CSS_URL
+}));
 // Middleware
 app.use(express.json());
 app.use(morgan("dev"));
